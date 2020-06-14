0fc853753e8df03e48179a0111b5104d5f9cb050
lesezhu <muhammad.masud.1997@gmail.com>
2020-06-14 13:31:40 +0100

    fix typo/missing data

--- SEIQRDP_model/__init__.py
+++ SEIQRDP_model/__init__.py
@@ -1,4 +1,4 @@
-from SEIQRDP_model.Algeria_SEIR_COVID2019_Object import Configurator, Simulator, Comparator, GeneticFit, Analyzer, BaseExperiment
-from SEIQRDP.DataCollector import Region, WebDataReader
-from SEIQRDP.Experiment import Experiment
-from SEIQRDP.SEIR_Solver import SEIRSolver
+from SEIQRDP_model.Algeria_SEIR_COVID2019_Object import Analyzer, Configurator, Simulator, Comparator, GeneticFit, Analyzer, BaseExperiment
+from SEIQRDP_model.DataCollector import Region, WebDataReader
+from SEIQRDP_model.Experiment import Experiment
+from SEIQRDP_model.SEIR_Solver import SEIRSolver
