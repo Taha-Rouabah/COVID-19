5e05f7008c606414d6b4973910816254acad3a1f
lesezhu <muhammad.masud.1997@gmail.com>
2020-06-14 11:11:03 +0100

    requirements/diag/init

---
+++ SEIQRDP_model/__init__.py
@@ -0,0 +1,4 @@
+from SEIQRDP_model.Algeria_SEIR_COVID2019_Object import Configurator, Simulator, Comparator, GeneticFit, Analyser, BaseExperiment
+from SEIQRDP.DataCollector import Region, WebDataReader
+from SEIQRDP.Experiment import Experiment
+from SEIQRDP.SEIR_Solver import SEIRSolver
