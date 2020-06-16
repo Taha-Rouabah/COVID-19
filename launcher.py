5648bf34c3d110a449cc10fbdc9e46b09325038e
Nacer eddine BELALOUI <65052452+Belaloui@users.noreply.github.com>
2020-06-16 18:36:30 +0100

    Fixed launcher.py
    Added [ __name__ == '__main__' ] condition to fix the multiprocessing bug on Windows.

--- launcher.py
+++ launcher.py
@@ -8,45 +8,38 @@ This is a quick sript to launch the SEIQRDP program
 from seiqrdp_model.Experiment import Experiment
 from datetime import datetime
 
+if __name__ == '__main__':
+    print("\n___________________ INFO___________________ \n")
 
-print("\n___________________ INFO___________________ \n")
+    # time info dd/mm/YY H:M:S
+    now = datetime.now()
+    dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(" date and time =", dt_string)
 
-# time info dd/mm/YY H:M:S
-now = datetime.now()
-dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-print(" date and time =", dt_string)
+    # Parameters
+    nExp = 12
+    mGen = 20
+    nDays = 90
 
-# Parameters
-nExp = 12
-mGen = 20
-nDays = 90
+    location = 'Italy'
 
-location = 'Italy'
+    # location = 'Spain'
 
-# location = 'Spain'
-
-print(f'\n This run simulates using internet available data of {location}, for {nDays} days, with {nExp} experiments and {mGen} maximum generations. \n ')
-
-
-# Run programs
-print("\n___________________ START___________________ \n")
-
-SEIQRDP_exp=Experiment(location,nDays, useMP=True)
-SEIQRDP_exp.run(nExp,mGen)
-
-# Show results
-SEIQRDP_exp.show_results()
-
-print("\n___________________ Done !___________________ \n")
+    print(f'\n This run simulates using internet available data of {location}, for {nDays} days, with {nExp} experiments and {mGen} maximum generations. \n ')
 
 
-now = datetime.now()
-dt_ending = now.strftime("%d/%m/%Y %H:%M:%S")
-print(" date and time =", dt_ending)
+    # Run programs
+    print("\n___________________ START___________________ \n")
+
+    SEIQRDP_exp=Experiment(location,nDays, useMP=True)
+    SEIQRDP_exp.run(nExp,mGen)
+
+    # Show results
+    SEIQRDP_exp.show_results()
+
+    print("\n___________________ Done !___________________ \n")
 
 
-
-
-
-
-
+    now = datetime.now()
+    dt_ending = now.strftime("%d/%m/%Y %H:%M:%S")
+    print(" date and time =", dt_ending)
