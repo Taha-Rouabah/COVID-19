d493445ccf20fedb69da95f584c12e2438dd3c0c
lesezhu <muhammad.masud.1997@gmail.com>
2020-06-18 12:13:23 +0100

    py req 3.6

--- setup.py
+++ setup.py
@@ -1,18 +1,20 @@
 
 import setuptools
 
+
 def get_requirements():
     reqs = []
     for line in open('requirements.txt', 'r').readlines():
         reqs.append(line)
     return reqs
 
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="seiqrdp_model", 
-    version="7.5.2",
+    name="seiqrdp_model",
+    version="7.5.3",
     author="T.Rouabah, N.Belaloui, A.Tounsi",
     author_email="m.t.rouabah@gmail.com",
     description="Computational tools to fit data, simulate and calibrate\
@@ -26,6 +28,6 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.6',
     install_requires=get_requirements()
 )
